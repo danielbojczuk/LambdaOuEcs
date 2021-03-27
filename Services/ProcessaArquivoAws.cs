274142b4ae87bd18479a88cf4f66e9e1b65ab4a8
Daniel Antonio Fernandes Bojczuk <danielbojczuk@gmail.com>
2021-03-27 14:55:04 -0300

    Atualização para armazenar tempos de processamento em Logs.

--- Services/ProcessaArquivoAws.cs
+++ Services/ProcessaArquivoAws.cs
@@ -3,6 +3,7 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using System.IO;
 using System;
+using System.Diagnostics;
 namespace Services
 {
     public class ProcessaArquivoAws : IProcessaArquivoAws
@@ -10,14 +11,28 @@ namespace Services
         private AmazonS3Client AwsClient;
         private string BucketName;
         private string ObjectKey;
+        private Stopwatch Temporizador;
+
+        private TimeSpan TempoSort;
+
+        private TimeSpan TempoRetornaArquivo;
+
+        public ProcessaArquivoAws() {
+            this.Temporizador = new Stopwatch();
+        }
 
         public async Task<string> Iniciar(string bucketName, string objectKey, string region) {
             this.BucketName = bucketName;
             this.ObjectKey = objectKey;
             this.AwsClient = new AmazonS3Client(Amazon.RegionEndpoint.GetBySystemName(region));
+            this.IniciarTimer();
             string texto =await this.RetornaArquivoTexto();
+            this.PararTimer(out this.TempoRetornaArquivo);
+            this.ReiniciarTimer();
             string[] linhas = texto.Split(new string[] { System.Environment.NewLine },StringSplitOptions.RemoveEmptyEntries);
             Array.Sort(linhas);
+            this.PararTimer(out this.TempoSort);
+            Console.WriteLine("Tempos,{0},{1}",this.TempoRetornaArquivo,this.TempoSort);
             return string.Join(Environment.NewLine,linhas);
         }
 
@@ -35,6 +50,21 @@ namespace Services
                 arquivoRetorno = sr.ReadToEnd();
             }
             return arquivoRetorno;
+        }
+
+        private void IniciarTimer() {
+            this.Temporizador.Start();
+        }
+
+        private void PararTimer(out TimeSpan tempoArmazenado) {
+            this.Temporizador.Stop();
+            tempoArmazenado = this.Temporizador.Elapsed;
         } 
+
+        private void ReiniciarTimer() {
+            this.Temporizador.Reset();
+            this.Temporizador.Start();
+        } 
+
     }
 }
